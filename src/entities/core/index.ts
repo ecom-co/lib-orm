b8d9f2381b1439641d7b1bc3f2994bc0e68c5368
Nam077 <nam077.work@gmail.com>
2025-08-09 22:44:50 +0700

    refactor: update CORE_ENTITIES type from EntityClassOrSchema to EntityTarget for improved type safety

--- src/entities/core/index.ts
+++ src/entities/core/index.ts
@@ -1,10 +1,10 @@
 export * from './user.entity';
 export * from './product.entity';
 
-import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
+import type { EntityTarget } from 'typeorm';
 
 import { Product } from './product.entity';
 import { User } from './user.entity';
 
 // Core entity list for easy registration in modules/datasource
-export const CORE_ENTITIES: EntityClassOrSchema[] = [User, Product];
+export const CORE_ENTITIES: EntityTarget<unknown>[] = [User, Product];
