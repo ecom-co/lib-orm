4f12c98d1316cdb35a167d3033fd9894c4c76345
Nam077 <nam077.work@gmail.com>
2025-08-09 22:41:45 +0700

    feat: add CORE_ENTITIES export for easy registration of User and Product entities

--- src/entities/core/index.ts
+++ src/entities/core/index.ts
@@ -1,2 +1,10 @@
 export * from './user.entity';
 export * from './product.entity';
+
+import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
+
+import { Product } from './product.entity';
+import { User } from './user.entity';
+
+// Core entity list for easy registration in modules/datasource
+export const CORE_ENTITIES: EntityClassOrSchema[] = [User, Product];
