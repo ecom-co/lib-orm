8e14f1f7fcfb5afbcb72b4c546f4b072d6adecf5
Nam077 <nam077.work@gmail.com>
2025-08-09 22:51:24 +0700

    refactor: restore explicit type annotation for CORE_ENTITIES to EntityClassOrSchema for improved clarity

--- src/entities/core/index.ts
+++ src/entities/core/index.ts
@@ -1,8 +1,10 @@
 export * from './user.entity';
 export * from './product.entity';
 
+import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
+
 import { Product } from './product.entity';
 import { User } from './user.entity';
 
 // Core entity list for easy registration in modules/datasource
-export const CORE_ENTITIES = [User, Product];
+export const CORE_ENTITIES: EntityClassOrSchema[] = [User, Product];
