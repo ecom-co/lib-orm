24181b8f4a53aead806e6b11f19553ff6cf70b89
Nam077 <nam077.work@gmail.com>
2025-08-09 22:01:38 +0700

    refactor: rename Example class to User and remove unused fields

--- src/entities/core/user.entity.ts
+++ src/entities/core/user.entity.ts
@@ -3,16 +3,10 @@ import { Column, Entity } from 'typeorm';
 import { OrmBaseEntity } from '../base.entity';
 
 @Entity({ name: 'users' })
-export class Example extends OrmBaseEntity {
+export class User extends OrmBaseEntity {
     @Column({ type: 'varchar', length: 255 })
     name!: string;
 
-    @Column({ type: 'text', nullable: true })
-    description?: string | null;
-
     @Column({ type: 'int', name: 'isActive', default: 1 })
     isActive!: boolean;
-
-    @Column({ type: 'numeric', precision: 12, scale: 2, nullable: true })
-    price?: number | null;
 }
