6ccef726c0e94093531d61d08d62da99507e8eaf
Nam077 <nam077.work@gmail.com>
2025-08-09 22:49:59 +0700

    refactor: simplify CORE_ENTITIES declaration by removing explicit type annotation

--- src/entities/core/index.ts
+++ src/entities/core/index.ts
@@ -1,10 +1,8 @@
 export * from './user.entity';
 export * from './product.entity';
 
-import type { EntityTarget } from 'typeorm';
-
 import { Product } from './product.entity';
 import { User } from './user.entity';
 
 // Core entity list for easy registration in modules/datasource
-export const CORE_ENTITIES: EntityTarget<unknown>[] = [User, Product];
+export const CORE_ENTITIES = [User, Product];
